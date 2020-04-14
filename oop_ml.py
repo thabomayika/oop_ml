32bfcb4f5abd34ebe966b3c31b6983db1496c710
Thabo Mayika <mayikathabo@gmail.com>
2020-04-14 13:10:37 +0200

    fourth commit

--- oop_ml.py
+++ oop_ml.py
@@ -14,9 +14,19 @@ class ErrorCalculator:
 
     def get_residuals(self):
         self.resid = self.obs_var - self.pred_var
+        return self.resid
 
     def get_standardised_residuals(self):
         self.stand_res = (self.obs_var - self.pred_var) / (self.pred_var)**0.5
+        return self.stand_res
 
     def get_mse(self):
         self.mse = mse(self.obs_var, self.pred_var)
+        return self.mse
+
+    def get_rmse(self):
+        self.rmse = mse(self.obs_var, self.pred_var, squared=False)
+        return self.rmse
+
+    @classmethod
+    def error_summary(cls, min, max)
